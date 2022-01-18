950663d33af1d238d434ddca879edda2757b19b9
Zach Lovin <zrlovin@gmail.com>
2022-01-17 20:46:32 -0800

    Add delta patching
    Cleanup typings

--- server/src/graphql/local/schema.ts
+++ server/src/graphql/local/schema.ts
@@ -1,7 +1,43 @@
 import { makeExecutableSchema } from "@graphql-tools/schema"
 import { BREAK, DocumentNode, GraphQLError, Kind, SelectionSetNode, visit } from "graphql"
 import { ExecutionResult } from "graphql-ws"
+import hasha from "hasha"
+import * as jsonDiffPatch from "jsondiffpatch"
+import * as jsondiffpatchArraysByHash from "jsondiffpatch-arrays-by-hash"
+import _ from "lodash"
 import { hasuraSchema as rawHasuraSchema, wsExecutor } from "../hasura/schema"
+
+const cache: any = {}
+const instance = jsonDiffPatch.create({
+    objectHash: (obj: any, idx: number) => {
+        if (_.has(obj, "id") && _.has(obj, "__typename")) {
+            return `${obj.__typename}:${obj.id}`
+        } else if (_.has(obj, "id")) {
+            return obj.id
+        } else if (_.has(obj, "__ref")) {
+            return obj.__ref
+        } else {
+            const newObj = _.chain(obj).toPairs().sortBy(0).fromPairs().value()
+            const hash = hasha(JSON.stringify(newObj), { algorithm: "md5" })
+            return hash
+        }
+    },
+    arrays: {
+        // default true, detect items moved inside the array (otherwise they will be registered as remove+add)
+        detectMove: true,
+        // default false, the value of items moved is not included in deltas
+        includeValueOnMove: false
+    }
+})
+
+instance.processor.pipes.diff.replace("arrays", jsondiffpatchArraysByHash.diffFilter)
+instance.processor.pipes.patch.replace("arrays", jsondiffpatchArraysByHash.patchFilter)
+instance.processor.pipes.patch.replace("arraysCollectChildren", jsondiffpatchArraysByHash.collectChildrenPatchFilter)
+instance.processor.pipes.reverse.replace("arrays", jsondiffpatchArraysByHash.reverseFilter)
+instance.processor.pipes.reverse.replace(
+    "arraysCollectChildren",
+    jsondiffpatchArraysByHash.collectChildrenReverseFilter
+)
 
 export const schema = makeExecutableSchema({
     typeDefs: [
@@ -18,8 +54,7 @@ export const schema = makeExecutableSchema({
 
             type LiveSubscription {
                 query: subscription_root
-                deltas: [delta]!
-                # patch: [RFC4627Patch]
+                deltas: [delta!]
             }
 
             type Subscription {
@@ -60,6 +95,9 @@ export const schema = makeExecutableSchema({
                         ]
                     }
 
+                    let rev = 0
+                    const docHash = hasha(JSON.stringify(info.operation), { algorithm: "md5" })
+
                     try {
                         const iterable = await wsExecutor({
                             document: op,
@@ -70,15 +108,33 @@ export const schema = makeExecutableSchema({
                             if (result.errors != null) {
                                 throw result.errors[0]
                             } else {
-                                yield {
-                                    live: {
-                                        query: result.data,
-                                        deltas: [
-                                            {
-                                                rev: "0",
-                                                patch: "5"
-                                            }
-                                        ]
+                                const source = rev === 0 ? {} : cache[docHash][cache[docHash].lastRev].fromServer
+                                const patch = instance.diff(source, result.data)
+
+                                if (patch != null) {
+                                    if (cache[docHash] == null) {
+                                        cache[docHash] = {}
+                                    }
+                                    cache[docHash].lastRev = rev
+                                    cache[docHash][rev] = {
+                                        fromServer: result.data,
+                                        patch: patch
+                                    }
+                                    rev += 1
+
+                                    yield {
+                                        live: {
+                                            query: instance.patch(
+                                                _.cloneDeep(source),
+                                                patch
+                                            ),
+                                            deltas: [
+                                                {
+                                                    rev: rev - 1,
+                                                    patch: JSON.stringify(patch)
+                                                }
+                                            ]
+                                        }
                                     }
                                 }
                             }
